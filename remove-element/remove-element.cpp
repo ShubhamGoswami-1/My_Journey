0f3a88118a1fedce73bfa3321ed84833cad664ce
ShubhamGoswami-1 <47947100+ShubhamGoswami-1@users.noreply.github.com>
2021-12-19 13:39:09 +0530

    Update remove-element.cpp

--- remove-element/remove-element.cpp
+++ remove-element/remove-element.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     /*
-    // its an O(nlogn) solution ....m looking for O(n)... Is it Possible ?
+     its an O(nlogn) solution ....m looking for O(n)... Is it Possible ?
     int removeElement(vector<int>& nums, int val) {
-        /*
+        
         this approach will only work when relative order doesnt matter 
         because i will first sort the array and then its easy to count the 
         occurences and also to shift all the other elements in the place
         of all vals ,coz after sorting i will get all the vals in one group
         This means i will return the array in sorted manner .
-        */
+        
         int count1=0,pos=0,i;
         for(i=0;i<nums.size();i++){
             if(nums[i]==val){
