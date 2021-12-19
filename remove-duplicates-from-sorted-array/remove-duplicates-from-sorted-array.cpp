90418e1fadee5a0174307bf9bb1212b4d9d328a5
ShubhamGoswami-1 <47947100+ShubhamGoswami-1@users.noreply.github.com>
2021-12-19 11:27:08 +0530

    Update remove-duplicates-from-sorted-array.cpp

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -5,11 +5,13 @@ public:
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]){
                 ++count1;
+                //counting the duplicate elements 
             }
             else{
                 nums[i-count1]=nums[i];
+                // and whenever we get a unique element we go back to the current index - no. of duplicate elements till now .
             }
         }
-        return nums.size()-count1;
+        return nums.size()-count1;// size - total number of duplicate elements .
     }
 };
