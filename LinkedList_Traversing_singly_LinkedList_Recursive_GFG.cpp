7e32e8340a4bbe4ff2e7e9926ec847cd143ec60e
ShubhamGoswami-1 <47947100+ShubhamGoswami-1@users.noreply.github.com>
2022-05-16 20:45:13 +0530

    Add files via upload

---
+++ LinkedList_Traversing_singly_LinkedList_Recursive_GFG.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+void print(Node *head){
+    if(head==NULL){
+        return ;
+    }
+    cout<<head->data<<" ";
+    print(head->next);
+}
+int main()
+{
+    Node *head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    print(head);
+    return 0;
+}
